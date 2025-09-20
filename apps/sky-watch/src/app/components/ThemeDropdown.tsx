a5867305861c44f37e44fbb1d77b2f70bec89ce7
arshabh-sky-watch <arshabh@sky-watch.io>
2025-09-20 13:40:07 +0530

    refactor(theme): update theme names to 'garden' and 'forest' in globals.css and ThemeDropdown for consistency

--- apps/sky-watch/src/app/components/ThemeDropdown.tsx
+++ apps/sky-watch/src/app/components/ThemeDropdown.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { useEffect, useState } from "react";
 
-const LIGHT_THEME = 'light';
-const DARK_THEME = 'dark';
+const LIGHT_THEME = 'garden';
+const DARK_THEME = 'forest';
 
 const ThemeDropdown = () => {
     const [theme, setTheme] = useState<'light' | 'dark'>('light');
