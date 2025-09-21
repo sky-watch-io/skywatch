3381f7119ed0e98077eec2be8b06c188874751e3
arshabh-sky-watch <arshabh@sky-watch.io>
2025-09-21 12:34:13 +0530

    refactor(schema): remove schema.ts file and update drizzle.config.ts to reference schema directory

--- apps/sky-watch/drizzle.config.ts
+++ apps/sky-watch/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     dialect: "postgresql",
-    schema: "./src/schema.ts",
+    schema: "./src/schema",
     out: "./drizzle",
     dbCredentials: {
         url: process.env.DATABASE_URL!
